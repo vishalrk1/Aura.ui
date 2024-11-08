10aba486b620a9310b7e2835a5070b20494f071c
rainasaxena <raina1.saxena@gmail.com>
2024-11-08 11:35:40 +0530

    Added Gradient Text .mdx File

--- components/displayComponents/Text/GradientText/GradientText.tsx
+++ components/displayComponents/Text/GradientText/GradientText.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
-const GradientText = () => {
+interface TextProps{
+    startColor : string,
+    endColor : string,
+    text : string,
+    className?: string
+}
+
+const GradientText: React.FC<TextProps> = ({startColor="#666666", endColor="#ffffff", text, className}) => {
   return (
-    <div>GradientText</div>
+    <h1
+      className={twMerge("text-5xl font-bold text-center",className)}
+      style={{
+        backgroundImage: `linear-gradient(to bottom, ${startColor}, ${endColor})`,
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+      }}
+    >
+      {text}
+    </h1>
   )
 }
 
